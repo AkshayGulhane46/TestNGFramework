cf1952a64f91e0652792afa898f92c8267b3cd72
Akshay Gulhane <argulhane10246@gmail.com>
2023-10-04 22:44:57 +0530

    Commit100923

--- src/test/java/com/recruitCRM/Contacts/deleteCompanyContactTC.java
+++ src/test/java/com/recruitCRM/Contacts/deleteCompanyContactTC.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class deleteCompanyContactTC extends ContactsBaseTest{
 
-    // Test to delete a existing company contact
+    // Test to delete a existing company contact from database
     @Test
     public void editCompanyContact() throws IOException, InterruptedException {
         loginWithValidUser();
